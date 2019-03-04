85b850799f1ff083a1d82faaa7c3ee2fc9769d39
einGlasRotwein <juliane.tkotz@gmail.com>
2019-03-04 07:58:26 +0100

    Updated description with credits (even if not readable by user in hidden function).

--- R/validate_input.R
+++ R/validate_input.R
@@ -1,20 +1,20 @@
 #' A function for input validation
-#' 
+#'
 #' @param obj The object that undergoes validation
-#' @param argument_name A string indicating the name of the object 
-#'   This name is used when an error is thrown so the user 
+#' @param argument_name A string indicating the name of the object
+#'   This name is used when an error is thrown so the user
 #'   is informed on the cause of the error.
-#' @param class_string A character vector of legal classes. If 
-#'   \code{class_string} is "numeric", it will be expanded to 
-#'   c("numeric", "integer", "double"). The class is tested via the 
-#'   function \code{class}. This means that if \code{obj} is a matrix, 
-#'   it is necessary to pass \code{class_string = "matrix"}; you cannot 
-#'   refer to the "mode" of the matrix. A special case is the 
-#'   \code{class_string} "groupvar" that is expanded to 
+#' @param class_string A character vector of legal classes. If
+#'   \code{class_string} is "numeric", it will be expanded to
+#'   c("numeric", "integer", "double"). The class is tested via the
+#'   function \code{class}. This means that if \code{obj} is a matrix,
+#'   it is necessary to pass \code{class_string = "matrix"}; you cannot
+#'   refer to the "mode" of the matrix. A special case is the
+#'   \code{class_string} "groupvar" that is expanded to
 #'   c("factor", "character", "numeric", "integer", "double").
 #' @param len Optional numeric vector for objects having a length
 #'   (mostly for vectors).
-#' @param gt0 Optional logical vector indicating if numeric input has 
+#' @param gt0 Optional logical vector indicating if numeric input has
 #'   to be greater than 0.
 #' @param must_be_integer Optional logical vector indicating if numeric
 #'   input has to be integer.
@@ -22,18 +22,22 @@
 #'   consist of.
 #' @param input_set Optional argument specifying a set of values an
 #'   argument can take.
-#' 
-#' @return NULL 
-#' 
+#'
+#' @details
+#' Taken from the package \code{\link{prmisc}} by Martin Papenberg.
+#' Git: https://github.com/m-Py/prmisc
+#'
+#' @return NULL
+#'
 #' @noRd
 
-validate_input <- function(obj, argument_name, class_string = NULL, len = NULL, 
+validate_input <- function(obj, argument_name, class_string = NULL, len = NULL,
                            gt0 = FALSE, must_be_integer = FALSE, groupsize = NULL,
                            input_set = NULL) {
-  
-  self_validation(argument_name, class_string, len, gt0, 
+
+  self_validation(argument_name, class_string, len, gt0,
                   must_be_integer, groupsize, input_set)
-  
+
   ## - Check class of object
   if (argument_exists(class_string))  {
     # Allow for all numeric types:
@@ -42,29 +46,29 @@ validate_input <- function(obj, argument_name, class_string = NULL, len = NULL,
     }
     # Case - grouping variable: Allow for numeric, character or factor
     if ("groupvariable" %in% class_string) {
-      class_string <- setdiff(c(class_string, "factor", "character", 
-                                "numeric", "integer", "double"), 
+      class_string <- setdiff(c(class_string, "factor", "character",
+                                "numeric", "integer", "double"),
                               "groupvariable")
     }
     correct_class <- class(obj) %in% class_string
     if (!correct_class) {
-      stop(argument_name, " must be of class '", 
+      stop(argument_name, " must be of class '",
            paste(class_string, collapse = "' or '"), "'")
     }
   }
-  
+
   ## - Check length of input
   if (argument_exists(len)) {
     if (length(obj) != len) {
       stop(argument_name, " must have length ", len)
     }
   }
-  
+
   ## - Check if input has to be greater than 0
   if (gt0 == TRUE && any(obj <= 0)) {
     stop(argument_name, " must be greater than 0")
   }
-  
+
   ## - Check if input has to be integer
   if (must_be_integer == TRUE && any(obj %% 1 != 0)) {
     stop(argument_name, " must be integer")
@@ -72,23 +76,23 @@ validate_input <- function(obj, argument_name, class_string = NULL, len = NULL,
   ## - Check if correct number of groups is provided
   if (argument_exists(groupsize)) {
     if (length(table(obj)[table(obj) != 0]) != groupsize) {
-      stop(argument_name, " must consist of exactly ", groupsize, 
+      stop(argument_name, " must consist of exactly ", groupsize,
            " groups with more than 0 observations.")
     }
   }
-  
+
   ## - Check if argument matches a predefined input set
   if (argument_exists(input_set)) {
     if (!obj %in% input_set) {
-      stop(argument_name, " can either be set to '", 
+      stop(argument_name, " can either be set to '",
            paste(input_set, collapse = "' or '"), "'")
     }
   }
-  
+
   return(invisible(NULL))
 }
 
-## Validate input for the `validate_input` function (these errors are 
+## Validate input for the `validate_input` function (these errors are
 ## not for users, but only for developers)
 self_validation <- function(argument_name, class_string, len, gt0,
                             must_be_integer, groupsize,
@@ -107,17 +111,17 @@ self_validation <- function(argument_name, class_string, len, gt0,
   stopifnot(length(gt0) == 1)
   stopifnot(class(must_be_integer) == "logical")
   stopifnot(length(must_be_integer) == 1)
-  
+
   if (argument_exists(groupsize)) {
     stopifnot(mode(groupsize) == "numeric")
     stopifnot(length(groupsize) == 1)
   }
-  
+
   if (argument_exists(input_set) && len != 1) {
     stop("If an input set is passed, argument len must be 1 ",
          "(this message should not be seen by users of the package prmisc).")
   }
-  
+
   return(invisible(NULL))
 }
 
